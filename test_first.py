80f9efeea362e7ca9411c8a47b1958c04a5a8a4a
Kostya Egorov <kosta.work0@gmail.com>
2025-02-26 20:57:40 +0700

    Add test function for Google search with screenshot capture

--- test_first.py
+++ test_first.py
@@ -1,8 +1,10 @@
 from selene import browser, be, have
 
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('html').should(have.text('About this page'))
+def test_google_search():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('html').should(have.text('Об этой странице'))
+    browser.driver.save_screenshot('screenshot.png')
 
-# browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    # browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
